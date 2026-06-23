c5ae82da58d11875c9c1ccc9d0558f178e1d77c3
aliciemachi <alice.machieraldo@gmail.com>
2026-06-23 21:18:46 +0200

    modifiche

--- upload_citation.py
+++ upload_citation.py
@@ -6,5 +6,27 @@ class Handler():
     def __init__(self):
         self.dbPathOrUrl = ""
 
+    # Methods
+    def getDbPAthOrUrl(self):
+        return self.dbPathOrUrl 
+     # it returns the path or URL of the database
+    
+    def setDbPathOrUrl(self, pathOrUrl: str):
+        self.dbPathOrUrl = pathOrUrl
+        return True
+     # it enables to set a new path or URL for 
+     # the database to handle
+
+class UploadHandler(Handler):
+    def __init__(self):
+        super().__init__()
+
+    # Methods
+    def pushDataToDb(self, path: str): 
+        pass
+
+
+
+
 
 
