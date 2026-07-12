62356aa65faba62af1b52c7175c9efafacfc33cc
Silvia <silviabaldassarri2001@gmail.com>
2026-07-12 16:50:00 +0200

    Un minimo di canovaccio super provvisorio di cosa voglio mettere nel mio impl

--- impl_s.py
+++ impl_s.py
@@ -1,1 +1,27 @@
-# vediamo se questo file ESISTE
+# Class Handler con attributi dbPathorUrl; metodi getDbpathorurl, set dbpathorurl
+class Handler:
+    def __init__(self):
+        self.DbPathOrUrl = str
+    
+    def getDbPathOrUrl(self):
+        return self.DbPathOrUrl
+    
+    #GIÀ QUI NON STO PIù CAPENDO COME FUNZIONA
+    def setDbPathOrUrl(self):
+        try:
+            self.DbPathOrUrl = DbPathOrUrl
+            return True
+        except Exception as e:
+            return False
+
+# Class uploadhandler con metodi pushdatatodb
+
+class UploadHandler(Handler):
+    def __init__(self):
+        super().__init__()
+
+    #@abstractmethod
+    #def PushDataToDb(self, path: str):
+    
+# Class bibliographichentityuploadhandler
+#Class bibliographichentityqueryhandler
